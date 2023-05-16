1ca2d0e545e997668fda4877e5940b07768424ee
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-16 21:59:57 +0300

    Update main page files

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+include 'header.php'
+?>
+<h1 class="fw-bold mt-5 fs-1">Kuafor Randevu Sistemi</h1>
+<ul class="navigation mx-auto border rounded-4">
+    <li class="rounded p-3"><a href="#">Giriş Yap</a></li>
+    <li class="rounded p-3"><a href="#">Randevular</a></li>
+    <li class="rounded p-3"><a href="#">Randevu Oluştur</a></li>
+    <li class="rounded p-3"><a href="#">Randevu Sil</a></li>
+    <li class="rounded p-3"><a href="#">Randevu Güncelle</a></li>
+</ul>
+
+<?php
+include_once 'footer.php'
+?>
