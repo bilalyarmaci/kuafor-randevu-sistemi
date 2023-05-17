4679d8a9b3a138fe35f793707a2bf8deb6b79688
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-17 17:53:36 +0300

    Add incorrect input messages

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-include_once './header.php'
+include_once './header.php';
 ?>
 <!-- Section: Design Block -->
 <style>
@@ -20,7 +20,7 @@ include_once './header.php'
 
 <!-- Jumbotron -->
 <div class="container py-4">
-    <div class="row g-0 align-items-center">
+    <div class="row g-0 mt-5 align-items-center">
         <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="card cascading-right" style="
             background: hsla(0, 0%, 100%, 0.55);
@@ -28,21 +28,21 @@ include_once './header.php'
             ">
                 <div class="card-body p-5 shadow-5 text-center">
                     <h2 class="fw-bold mb-5">Kayıt Ol</h2>
-                    <form action="includes/signup-inc.php" method="post">
+                    <form action="./includes/signup-inc.php" method="post">
                         <!-- 2 column grid layout with text inputs for the first and last names -->
                         <div class="row">
                             <div class="col-md-6 mb-4">
                                 <div class="form-outline">
                                     <label class="form-label d-block text-start" for="ad">
                                         <i class="bi bi-person-badge-fill me-2"></i>Ad</label>
-                                    <input type="text" id="ad" class="form-control" placeholder="Adınız" />
+                                    <input type="text" id="ad" class="form-control" placeholder="Adınız" name="name" required />
                                 </div>
                             </div>
                             <div class="col-md-6 mb-4">
                                 <div class="form-outline">
                                     <label class="form-label d-block text-start" for="soyad">
                                         <i class="bi bi-person-badge me-2"></i>Soyad</label>
-                                    <input type="text" id="soyad" class="form-control" placeholder="Soyadınız" />
+                                    <input type="text" id="soyad" class="form-control" placeholder="Soyadınız" name="surname" />
                                 </div>
                             </div>
                         </div>
@@ -51,7 +51,7 @@ include_once './header.php'
                         <div class="form-outline mb-4">
                             <label class="form-label d-block text-start" for="mail">
                                 <i class="bi bi-envelope-at me-2"></i>Email</label>
-                            <input type="email" id="mail" class="form-control" placeholder="Email adresiniz" />
+                            <input type="email" id="mail" class="form-control" placeholder="Email adresiniz" name="email" required />
 
                         </div>
 
@@ -59,20 +59,34 @@ include_once './header.php'
                         <div class="form-outline mb-4">
                             <label class="form-label d-block text-start" for="pass">
                                 <i class="bi bi-key-fill me-2"></i>Şifre</label>
-                            <input type="password" id="pass" class="form-control" placeholder="Şifreniz" />
+                            <input type="password" id="pass" class="form-control" placeholder="Şifreniz" name="password" required />
                         </div>
 
                         <div class="form-outline mb-4">
-                            <label class="form-label d-block text-start" for="pass">
+                            <label class="form-label d-block text-start" for="passRepeat">
                                 <i class="bi bi-key me-2"></i>Şifre Tekrar</label>
-                            <input type="password" id="pass" class="form-control" placeholder="Tekrar şifreniz" />
+                            <input type="password" id="passRepeat" class="form-control" placeholder="Tekrar şifreniz" name="passwordRepeat" required />
                         </div>
 
                         <!-- Submit button -->
-                        <button type="submit" class="btn btn-lg btn-dark btn-block mb-4">
+                        <button type="submit" class="btn btn-lg btn-dark btn-block mb-4" name="submit">
                             Kayıt ol
                         </button>
                     </form>
+                    <!-- Hata oluşması halinde gerekli mesaj kullanıcıya iletilmekte -->
+                    <?php
+                    if (isset($_GET["error"])) {
+                        if ($_GET["error"] == "emptyinput") {
+                            echo '<h4 class="text-danger fw-bold">Lütfen zorunlu alanları doldurunuz!</h4>';
+                        } else if ($_GET["error"] == "emailexists") {
+                            echo '<h4 class="text-danger fw-bold">Bu email ile zaten kayıt mevcut!</h4>';
+                        } else if ($_GET["error"] == "nopwdsmatch") {
+                            echo '<h4 class="text-warning fw-bold">Şifrelerin uyuştuğundan emin olun.</h4>';
+                        } else if ($_GET["error"] == "stmtfail") {
+                            echo '<h4 class="text-warning fw-bold">Bir hata oluştu!</h4>';
+                        }
+                    }
+                    ?>
                 </div>
             </div>
         </div>
@@ -85,5 +99,5 @@ include_once './header.php'
 <!-- Jumbotron -->
 
 <?php
-include_once './footer.php'
+include_once './footer.php';
 ?>
