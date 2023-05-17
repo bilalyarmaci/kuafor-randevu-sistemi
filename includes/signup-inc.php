1fa7ef5b8bf82fd3ea373af5dff58b02973f291e
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-17 17:54:45 +0300

    Create signup functionality

---
+++ includes/signup-inc.php
@@ -0,0 +1,28 @@
+<?php
+require_once './functions-inc.php';
+
+if(!isset($_POST["submit"])){
+    header("Location: ../signup.php");
+    exit();
+}
+
+$uName = $_POST["name"];
+$uSurname = $_POST["surname"];
+$uEmail = $_POST["email"];
+$uPwd = $_POST["password"];
+$uPwdRpt = $_POST["passwordRepeat"];
+
+if(isEmptyInput($uName, $uEmail, $uPwd, $uPwdRpt) !== false){
+    header("Location: ../signup.php?error=emptyinput");
+    exit();
+}
+if(emailExists($connection, $uEmail) !== false){
+    header("Location: ../signup.php?error=emailexists");
+    exit();
+}
+if(noPwdsMatch($uPwd, $uPwdRpt) !== false){
+    header("Location: ../signup.php?error=nopwdsmatch");
+    exit();
+}
+
+createUser($connection, $uName, $uSurname, $uEmail, $uPwd);
