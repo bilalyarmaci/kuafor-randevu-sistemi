a76732d70ef595deb8a6e41cd86934d13c55d443
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-17 11:06:49 +0300

    Design improvements

--- main.php
+++ main.php
@@ -1,13 +1,14 @@
 <?php
 include 'header.php'
 ?>
-<h1 class="fw-bold mt-5 fs-1">Kuafor Randevu Sistemi</h1>
-<ul class="navigation mx-auto border rounded-4">
+
+<h1 class="fw-bold mt-5 p-3 fs-1">Kuaför Randevu Sistemi</h1>
+<ul class="navigation mx-auto shadow-lg border rounded-4">
     <li class="rounded p-3"><a href="#">Giriş Yap</a></li>
     <li class="rounded p-3"><a href="#">Randevular</a></li>
     <li class="rounded p-3"><a href="#">Randevu Oluştur</a></li>
-    <li class="rounded p-3"><a href="#">Randevu Sil</a></li>
     <li class="rounded p-3"><a href="#">Randevu Güncelle</a></li>
+    <li class="rounded p-3"><a href="#">Randevu Sil</a></li>
 </ul>
 
 <?php
