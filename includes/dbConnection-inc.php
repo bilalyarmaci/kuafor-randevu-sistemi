331e07b4693617aab6297de83cb56a62d3377361
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-17 15:47:56 +0300

    Add MySQL connection

---
+++ includes/dbConnection-inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'admin_panel';
+
+$connection = mysqli_connect($hostname,$username,$password,$db);
+
+// Bağlantı hatası olması durumunda
+if(!$connection){
+    die('MySQL sunucu ile baglanti kurulamadi! <br>HATA: ' . mysqli_connect_error());
+}
