0f4767dcd364147489e65532e0081771e38375ed
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2023-05-24 05:20:53 +0300

    Add favicon

--- header.php
+++ header.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
-?>
+echo '
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kuafor Randevu Sistemi</title>
+    <link rel="icon" type="image/x-icon" href="./imgs/favicon.ico">
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
@@ -18,4 +19,5 @@ session_start();
 </head>
 
 <body>
-    <div class="mx-auto">
+    <div class="mx-auto">';
+?>
